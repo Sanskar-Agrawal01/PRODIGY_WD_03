e5b5044eaa57e6d50081f606058f108722476b94
Sanskar Agrawal <agrawalsanskar54@gmail.com>
2024-06-04 23:50:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PRODIGY_WD_03
+ This is a TIC_TAC_TOE game made with JAVASCRIPT.
---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+ 
+  
+ const Button = ({ resetGame }) => {
+    return <button onClick={() => resetGame()}>New Game</button>;
+};
+
+export default Button;
